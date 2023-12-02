ec19c6cf70027c4d4e605fd91091ad69e6bdf464
Ric Dodong <ricalgen@gmail.com>
2023-12-02 16:45:28 +0800

    Update cms.js

--- src/js/cms.js
+++ src/js/cms.js
@@ -14,7 +14,7 @@ import FooterPreview from "./cms-preview-templates/footer";
 CMS.registerPreviewStyle(styles, {raw: true});
 CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
-CMS.registerPreviewTemplate("sesrmons", SermonsPreview);
+CMS.registerPreviewTemplate("sermons", SermonsPreview);
 CMS.registerPreviewTemplate("values", ValuesPreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
 CMS.registerPreviewTemplate("footer", FooterPreview);
